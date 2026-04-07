0503cf2f5e42b95a71d4b3f424f83d37d4b322a6
Eulla Oliveira <92802201+EullaOliveira@users.noreply.github.com>
2026-04-07 11:25:21 -0300

    Add files via upload

---
+++ Python/exercicio.py
@@ -0,0 +1,87 @@
+#print('Olá, Mundo!')
+#print('Eulla Paula dos Santos Oliveira')
+#print('Nome: Ana\nIdade: 15\nCurso: Infoweb')
+#print('1,2,3,4,5')
+#print("*****")
+#print("*   *")
+#print("*****")
+#print('Phyton é incrível!', end="")
+#print(10, 20, sep=" ")
+#for i in range(1, 6):
+#    print(2 * i)
+#print(f"{'Produto':<10} {'Preço'}")
+#print(f"{'Caneta':<10} {2.50:>.2f}")
+#print(f"{'Caderno':<10} {12.00:>.2f}")
+#print(f"{'Borracha':<10} {1.75:>.2f}")
+#cidade = "Natal,"
+#estado = "RN"
+#print('Eu moro em', cidade, estado)
+#ano_nascimento = "2011"
+#print('O ano de nascimento é:', ano_nascimento)
+
+#a = 5
+#b = 10
+#a, b = b, a
+#print('A =', a)
+#print('B =', b)
+
+x = 1
+y = 2
+z = 3
+soma = x + y + z
+print('A soma dos números de x, y e z é:', soma)
+
+mensagem = 'Aprendendo Phyton'
+print(mensagem)
+
+preco = '49,90'
+print('O preço do produto é: R$', preco)
+
+ativo = True
+print(ativo)
+
+pontos = 100
+print(pontos + 50)
+
+nome1 = 'Eulla'
+idade = '15'
+nota = '0'
+print('Aluno:', nome1, '\nIdade:', idade, '\nNota:', nota)
+
+#nome = input("Digite seu nome: ")
+#print(f"Olá, {nome}!")
+
+#cidade2 = input("Qual a sua cidade?")
+#print(f'Ótimo, você mora em {cidade2}.')
+
+#numero1 = input("Digite o primeiro número: ")
+#numero2 = input("Digite o segundo número: ")
+#print(numero1)
+#print(numero2)
+
+#nome2 = input("Qual o seu nome? ")
+#idade2 = input("Quantos anos você tem? ")
+#print('Usuário', nome2, 'tem', idade2, 'anos.')
+
+#ano2 = input('Digite o seu ano de nascimento: ')
+#print('Você nasceu no ano', ano2)
+
+#palavra1 = input('Digite uma palavra: ')
+#print('Você digitou:', palavra1)
+
+#produto1 = input('Qual o nome o produto? ')
+#preco1 = input('Qual o preço do produto? ')
+#print("Produto:", produto1, "\nPreço: R$", preco1)
+
+#numero3 = input("Digite um númeo inteiro: ")
+#print(numero3)
+#numero_float = float(numero3)
+#print('O número digitado, em float é:', numero_float)
+
+#numero4 = int(input("Digite um número: "))
+#numero5 = int(input('Digite outro número: '))
+#print("A soma dos números digitados é:", numero4+numero5)
+
+num1 = (input("Digite um número: "))
+num2 = (input('Digite outro número: '))
+print("A soma dos números digitados é:", num1+num2)
